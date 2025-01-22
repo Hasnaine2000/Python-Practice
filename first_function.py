977e9682f821e92a6d9654e19c0fb215c415953e
KaziAbdullah <119121503+Hasnaine2000@users.noreply.github.com>
2025-01-22 20:58:58 +0600

    Day 2

---
+++ first_function.py
@@ -0,0 +1,9 @@
+x= "str"
+
+def myfunction():
+      print(x)
+      print("This is my first fuction!")
+
+
+myfunction()
+myfunction()
---
+++ tuple_set_list.py
@@ -0,0 +1,14 @@
+x = ("ami","janina","kichu") #tuple
+y = {"mane", "ki", "Bhai?"} #set
+z = ["keno","jano", "na"]#list
+
+print(x,y,z)
+
+zz = range(6)
+print(zz)
+
+yy = True
+print(yy)
+
+pp = tuple (z) #list to tuple
+print(pp) 
